0c9dee6937f146afbb500f6e6d8bf42ee7576096
Andriy03092001 <cuanid236316@gmail.com>
2020-09-02 18:22:11 +0300

    first

---
+++ Store/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Всіх
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //Лише адміни
+        [Authorize(Roles = "Admin")]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+
+        //Просто залогінився
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
